407a633f280817b9684ca124011903251faad280
victor manzanero <vctr.31@hotmail.com>
2021-01-18 06:00:31 -0600

    primer commit

---
+++ FlinkEntrevista/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlinkEntrevista
+//
+//  Created by victor manzanero on 18/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
